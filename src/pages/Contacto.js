1692b4c7b18a5284617cd4a6c44a8ff939b3e4d4
Genemichael25 <genemichael25@yahoo.com>
2022-12-08 16:05:43 -0700

    File structure set

---
+++ src/pages/Contacto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contacto = () => {
+  return (
+    <>
+      <h1 className='font-bold my-96'>
+        Contacto
+      </h1>
+    </>
+  )
+}
+
+export { Contacto }
---
+++ src/pages/Historia.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Historia = () => {
+  return (
+    <>
+      <h1 className='font-bold my-96'>
+        Historia
+      </h1>
+    </>
+  )
+}
+
+export { Historia }
