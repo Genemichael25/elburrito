719889fae0eef118e37e0a3b93adfad60c2a7027
Genemichael25 <genemichael25@yahoo.com>
2023-05-03 13:17:14 -0700

    Added image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
       <a href="/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo"/>
+          <img src="./Burrito.png" className="h-6 mr-3 sm:h-9" alt="Business Logo"/>
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">El Burrito.kom</span>
       </a>
       <div className="flex md:order-2">
